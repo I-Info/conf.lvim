0ceaa1e1f34ab51920199d00f5463cf8c302896c
I-Info <info@i1nfo.com>
2022-11-21 14:47:02 +0800

    fix keymapping

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -4,10 +4,12 @@ lvim.leader = "space"
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 lvim.keys.normal_mode["<A-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<A-h>"] = ":BufferLineCyclePrev<CR>"
-lvim.keys.normal_mode["Fb"] = ":BufferLinePick<CR>"
+lvim.keys.normal_mode[",b"] = ":BufferLinePick<CR>"
 
-vim.keymap.set({ 'n', 'v' }, 'Fl', require("user.leap").leap_to_line)
+vim.keymap.set({ 'n', 'v' }, ',l', require("user.leap").leap_to_line)
 vim.keymap.set({ 'n', 'i', 'v' }, '<C-s>', require("user.leap").leap_bidirectional)
+
+vim.keymap.set({ 'i' }, '<C-b>', lvim.builtin.cmp.mapping["<C-Space>"])
 
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -28,6 +28,25 @@ lvim.plugins = {
       }
     end
   },
-  -- Disabled Plgins
+  { "zbirenbaum/copilot.lua",
+    event = { "VimEnter" },
+    config = function()
+      vim.defer_fn(function()
+        require("copilot").setup {
+          plugin_manager_path = get_runtime_dir() .. "/site/pack/packer",
+        }
+      end, 100)
+    end,
+  },
+
+  { "zbirenbaum/copilot-cmp",
+    after = { "copilot.lua", "nvim-cmp" },
+    config = function()
+      require("copilot_cmp").setup()
+    end
+  }, -- Disabled Plgins
   -- { "mg979/vim-visual-multi" },
 }
+
+lvim.builtin.cmp.formatting.source_names["copilot"] = "(Copilot)"
+-- table.insert(lvim.builtin.cmp.sources, 1, { name = "copilot" })
